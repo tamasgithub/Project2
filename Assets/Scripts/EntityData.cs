df524089ba5e06df494c5704c05fe479b318fcca
Natsutobi <natsutobi28@gmail.com>
2026-06-20 19:42:13 +0200

    Modified EntityData

--- Assets/Scripts/EntityData.cs
+++ Assets/Scripts/EntityData.cs
@@ -1,16 +1,40 @@
+using System;
+using System.Collections.Generic;
 using Mirror;
 
 public class EntityData
 {
 
-    public int MaxHp { get; private set; } = 1;
-    public int Hp { get; private set; } = 1;
+    public int MaxHp { get; } = 1;
+
+    public int Hp { get; private set; }
+
     public float MovementSpeed { get; private set; } = 1f;
-    
-    public EntityData(int maxHp, float movementSpeed)
+    public float CDR { get; private set; } = 0.0f;
+    public int Damage { get; private set; } = 0;
+    public float ProjectileSize { get; set; } = 1.0f;
+    public EntityData(int maxHp, float movementSpeed, float cdr = 0.0f, int damage = 0, float projected = 1.0f)
     {
         MaxHp = maxHp;
         Hp = maxHp;
         MovementSpeed = movementSpeed;
     }
+
+
+    void SetHp(int hp)
+    {
+        Hp = Math.Clamp(hp, 0, MaxHp);
+    }
+    void ReceiveDamage(int amount)
+    {
+        //Damage Modifiers
+        SetHp(Hp - amount);
+    }
+    
+    void Heal(int amount)
+    {
+        //Heal Modifiers
+         SetHp(Hp + amount);
+    }
+
 }
