4224ebc024a6191938179a3443647d204b456724
Natsutobi <natsutobi28@gmail.com>
2026-06-20 18:48:02 +0200

    minor changes

--- Assets/Scripts/Ability/DaggerProjectile.cs
+++ Assets/Scripts/Ability/DaggerProjectile.cs
@@ -5,11 +5,17 @@ public class DaggerProjectile : NetworkBehaviour
 {
     public float speed;
     public Vector2 direction;
+    private float lifeTime;
 
     [Server]
     void Update()
     {
         if (!authority) return;
-        transform.position += (Vector3) direction * speed * Time.deltaTime;          
+        transform.position += (Vector3)direction * speed * Time.deltaTime;
+        lifeTime += Time.deltaTime;
+        if(lifeTime >= 10.0f)
+        {
+            NetworkServer.Destroy(gameObject);
+        }      
     }
 }
