1dc62eeb953ca712a790af9c491847a9b1b99741
tamasgithub <38605940+tamasgithub@users.noreply.github.com>
2026-07-20 20:35:11 +0200

    New player moving command / client rpc

--- Assets/Scripts/Player/PlayerInputController.cs
+++ Assets/Scripts/Player/PlayerInputController.cs
@@ -2,6 +2,7 @@ using System;
 using Mirror;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class PlayerInputController : NetworkBehaviour
 {
@@ -11,19 +12,22 @@ public class PlayerInputController : NetworkBehaviour
 
     void Update()
     {
-        if (isOwned)
+        if (true || isOwned)
         {
+            //Debug.Log("in update, isOwned: " + isOwned);
+            Debug.Log(InputSystem.actions.FindAction("move").ReadValue<Vector2>());
             CmdMovePlayer(
-                InputSystem.actions.FindAction("move").ReadValue<Vector2>()
+               InputSystem.actions.FindAction("move").ReadValue<Vector2>()
             );
-        }
+            if (isServer)
+            {
+                /*transform.position +=
+                    (Vector3)moveInput *
+                    GetComponent<Player>().MovementSpeed *
+                    Time.deltaTime;
+                */
+            }
 
-        if (isServer)
-        {
-            transform.position += 
-                (Vector3)moveInput *
-                GetComponent<Player>().MovementSpeed *
-                Time.deltaTime;
         }
     }
 
@@ -31,13 +35,23 @@ public class PlayerInputController : NetworkBehaviour
     [Command]
     private void CmdMovePlayer(Vector2 input)
     {
+        Debug.Log("CmdMovePlayer " +  input);
         moveInput = input;
         if(moveInput.magnitude > 0)
         {
             _faceDirection = moveInput.normalized; 
         }
        
-        
+        Vector2 newPosition = transform.position + ((Vector3)(input) * GetComponent<Player>().MovementSpeed * Time.deltaTime);
+        UpdatePosition(newPosition);
+    }
+
+
+    [ClientRpc]
+    private void UpdatePosition(Vector2 position)
+    {
+        Debug.Log("UpdatePosition to " + position);
+        transform.position = position;
     }
 
     public Vector2 FaceDirection()
